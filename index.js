de8ff6520b54b879589fbb62a99eb36f95f90f5f
makbule özbay <makbuleozbay8@gmail.com>
2019-07-21 18:18:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride <=400){result='This one is on me!'}
+  else if (ride<2500) {result='I will gladly take your thirty bucks.';}
+  else if (ride>2500){result='No can do.';}
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  var result = (city=="NYC") ? "Ok, sounds good.":"No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+       return 'Thank you.';
+    case 'thanks for everything':
+        return 'Bye.';
+  }
+  
 }
